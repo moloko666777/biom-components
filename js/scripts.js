d71076bf77f704cf38c8a6863cb6649d0235d1f8
moloko666777 <moloko666777@gmail.com>
2022-10-24 10:58:59 +0300

    add style for form

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 });
 
 const dropArea = document.querySelector(".drag-area"),
-    dragText = dropArea.querySelector("header"),
+    dragText = dropArea.querySelector(".drag-area-title"),
     button = dropArea.querySelector("button"),
     input = dropArea.querySelector("input");
 let file; //this is a global variable and we'll use it inside multiple functions
@@ -78,3 +78,52 @@ function showFile(){
         dragText.textContent = "Drag & Drop to Upload File";
     }
 }
+
+
+[].forEach.call(document.querySelectorAll('.input-phone input'), function (input) {
+    let keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        let pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        let matrix = "+3 80__-____-___",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            newValue = matrix.replace(/[_\d]/g, function (a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+            });
+        i = newValue.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            newValue = newValue.slice(0, i);
+        }
+        let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function (a) {
+                return "\\d{1," + a.length + "}";
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = newValue;
+        if (event.type == "blur" && this.value.length < 5) this.value = "";
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+});
+
+const inputs = document.querySelectorAll('.input-name')
+inputs.forEach(input => {
+    input.addEventListener('keydown', (event) => {
+        if (!/[A-Za-zА-Яа-я]/g.test(event.key)){
+            event.preventDefault()
+        }
+        if(length === 1 ) {
+            inputs.classList.add('error')
+        } else {
+            input.classList.remove('error')
+        }
+    })
+})
+
